e520f0475f08d18dc06c38bed5327560aa793ba6
PisarevskiyRE <daimon--@live.ru>
2024-05-29 22:57:23 +0300

    part 11 25-21

--- src/main/scala/homegrown/collections/FoldableFactory.scala
+++ src/main/scala/homegrown/collections/FoldableFactory.scala
@@ -1,5 +1,18 @@
 package homegrown.collections
 
-trait FoldableFactoryp[+Element] extends Foldable[Element]{
+trait FoldableFactory[+Element] extends Foldable[Element]{
+
+  protected def empty: FoldableFactory[Nothing]
+
+  def add[Super >: Element](input: Super): FoldableFactory[Super]
+
+
+  def map[Result](function: Element => Result): FoldableFactory[Result] = {
+    fold[FoldableFactory[Result]](empty) { (acc, current) =>
+      acc.add(function(current))
+    }
+  }
+
+
 
 }
