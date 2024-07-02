4451c1f2019822b67aa212368631d524122fa3e6
PisarevskiyRE <daimon--@live.ru>
2024-07-02 23:20:01 +0300

    part 17 start

--- src/main/scala/homegrown/collections/Stack.scala
+++ src/main/scala/homegrown/collections/Stack.scala
@@ -1,10 +1,6 @@
 package homegrown.collections
 
-
-
-
-
-sealed abstract class Stack[+Element]{
+sealed abstract class Stack[+Element] {
   import Stack._
 
   final def push[Super >: Element](input: Super): Stack[Super] =
@@ -12,7 +8,7 @@ sealed abstract class Stack[+Element]{
 
   final lazy val (peek, pop) = popElement
 
-  final def popElement: (Option[Element], Stack[Element])=
+  final def popElement: (Option[Element], Stack[Element]) =
     this match {
       case Empty =>
         None -> empty
@@ -22,59 +18,32 @@ sealed abstract class Stack[+Element]{
 
 }
 
-object Stack{
+object Stack {
   final case class NonEmpty[+Element](element: Element, otherElements: Stack[Element]) extends Stack[Element]
   final case object Empty extends Stack[Nothing]
 
   def empty: Stack[Nothing] = Empty
 }
 
+object Main extends App {
 
+  println("-" * 50)
 
+  println {
+    Stack.NonEmpty(
+      1, Stack.NonEmpty(
+        2, Stack.NonEmpty(
+          3, Stack.Empty
+        )
+      )
+    )
+  }
 
+  println(
+    Stack.Empty.push(3).push(2).push(1)
+  )
 
-//object Main extends App {
-//
-//  println("-" * 50)
-//
-//  val max = 15000
-//  println(max)
-//
-//  {
-//    var acc = 0
-//    while (acc < max) {
-//      acc += 1
-//    }
-//    println(acc)
-//  }
-//
-//  {
-//    @tailrec
-//    def loop(acc: Int): Int = {
-//      if (acc < max)
-//        loop(acc + 1)
-//      else
-//        acc
-//    }
-//
-//    println(loop(acc = 0))
-//  }
-//
-//  {
-//    //@tailrec
-//    def loop(acc: Int): Int = {
-//      if (acc < max)
-//        loop(acc + 1) + 0
-//      else
-//        acc
-//    }
-//
-//    println(loop(acc = 0))
-//  }
-//
-//  println("-" * 50)
-//
-//}
+  println("-" * 50)
 
-
+}
 
